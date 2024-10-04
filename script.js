1af3a4ec0c3a60adc8e6e31d1c0ce1ebf7553c55
HanzalaKhattak <muhammadhanzalahkhan1@gmail.com>
2024-10-04 11:09:28 +0500

    Added JS code to remove the error. the error was that 3 more days were shown to me

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ function calculateAge(){
         var year = today.getFullYear() - date.getFullYear();
         var month = today.getMonth() - date.getMonth();
         var day =  today.getDate() - date.getDate();
+        
+
+        if (month < 0 || (month === 0 && day < 0)) {
+            year = year -1;
+            month += 12;
+        }
+    
+        if (day < 0) {
+            month = month -1;
+            var previousMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+            day += previousMonth; 
+        }
+
 
         document.getElementById('year').innerHTML = year;
         document.getElementById('month').innerHTML = month;
