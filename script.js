fef7c680028370ddc9e55a4e9027b30083338e3f
HanzalaKhattak <muhammadhanzalahkhan1@gmail.com>
2024-10-04 08:48:41 +0500

    Removed errors from JS to display Exact Years of age and added styling

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function calculateAge(){
     const doB = document.getElementById('input').value;
     if(!doB){
-        alert("Select your Date of Birth first!");
-    }
+        alert("Select a Valid Date of Birth!");
+    }else{
+        const date = new Date(doB);
+        const today = new Date();
+        document.getElementById("year").innerHTML = today.getFullYear() - date.getFullYear();
+    }     
 }
