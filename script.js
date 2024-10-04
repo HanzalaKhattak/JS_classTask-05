9dd114b129c34167b805b36f56c7c4b0da2a3e21
HanzalaKhattak <muhammadhanzalahkhan1@gmail.com>
2024-10-04 10:59:38 +0500

    Added JS code for showing the values of age on the main window

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ function calculateAge(){
         var year = today.getFullYear() - date.getFullYear();
         var month = today.getMonth() - date.getMonth();
         var day =  today.getDate() - date.getDate();
-        
 
+        document.getElementById('year').innerHTML = year;
+        document.getElementById('month').innerHTML = month;
+        document.getElementById('day').innerHTML = day;
     }else{
         alert("Select a Valid Date of Birth!");
     }     
